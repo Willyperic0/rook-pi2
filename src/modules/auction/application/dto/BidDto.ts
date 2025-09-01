8186b056cc370bcbba77d836aae5d13f492ae73e
theweregh <luisalfredofuentesromero5@gmail.com>
2025-09-01 13:17:59 -0500

    update BidDto

--- src/modules/auction/application/dto/BidDto.ts
+++ src/modules/auction/application/dto/BidDto.ts
@@ -5,13 +5,3 @@ export interface BidDto {
   amount: number;
   timestamp: string; // ISO
 }
-
-const bid: BidDto = {
-  id: 1,
-  auctionId: 123,
-  userId: 45,
-  amount: 500,
-  timestamp: new Date().toISOString(),
-};
-
-const success = auction.placeBid(bid);
